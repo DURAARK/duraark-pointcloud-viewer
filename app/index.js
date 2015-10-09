7df6591e81222644d8c88509d3a107d7eb58e938
Martin Hecher <martin.hecher@fraunhofer.at>
2015-10-09 22:08:28 +0200

    added static express server

---
+++ app/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 9000,
+    publicDir = process.argv[2] || __dirname + '/../public';
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);
